e160f0639e196524aa8e70d74618b32776a3ac0d
FrozenIce0617 <prosdev087@gmail.com>
2019-10-21 13:45:33 -0400

    :sparkles: Add Priority Badge

--- src/components/ContractCard/index.js
+++ src/components/ContractCard/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { Badge } from 'react-native-elements';
 import { Dropdown } from 'react-native-material-dropdown';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -11,15 +12,9 @@ import { colors } from '../../styles';
 class ContractCard extends React.Component {
   render() {
     const { intentOptions, priorityOptions, info, onPressStretch } = this.props;
-    const borderColor =
-      info.priority === 'HIGH'
-        ? '#d03030'
-        : info.priority === 'MED'
-        ? '#803030'
-        : '#303030';
 
     return (
-      <View style={{ ...styles.container, borderColor }}>
+      <View style={styles.container}>
         {/* <View style={styles.row}>
           <View style={styles.row}>
             <View style={styles.category}>
@@ -50,8 +45,7 @@ class ContractCard extends React.Component {
         <View style={styles.rawText}>
           <Text>{info.rawtext}</Text>
         </View>
-
-        <View style={[styles.row, styles.flexEnd]}>
+        <View style={[styles.row, styles.footer]}>
           {/*
           <View style={styles.section}>
             <View style={styles.icon}>
@@ -73,6 +67,34 @@ class ContractCard extends React.Component {
 
           </View>
           */}
+          <View style={styles.section}>
+            {/* <Text>Priority: </Text>
+            <View style={{ ...styles.priority, backgroundColor }} /> */}
+            {info.priority === 'HIGH' && (
+              <Badge
+                value="HIGH"
+                status="error"
+                badgeStyle={{ width: 60, height: 25, borderRadius: 12 }}
+                textStyle={{ fontSize: 16, fontWeight: 'bold' }}
+              />
+            )}
+            {info.priority === 'MED' && (
+              <Badge
+                value="MED"
+                status="warning"
+                badgeStyle={{ width: 60, height: 25, borderRadius: 12 }}
+                textStyle={{ fontSize: 16, fontWeight: 'bold' }}
+              />
+            )}
+            {info.priority === 'LOW' && (
+              <Badge
+                value="LOW"
+                status="success"
+                badgeStyle={{ width: 60, height: 24, borderRadius: 12 }}
+                textStyle={{ fontSize: 16, fontWeight: 'bold' }}
+              />
+            )}
+          </View>
           <View style={styles.section}>
             <View style={styles.icon}>
               <TouchableOpacity onPress={onPressStretch}>
