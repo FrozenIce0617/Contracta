bb0362885314477b3b34547e4d56d5406a6b1df1
FrozenIce0617 <prosdev087@gmail.com>
2019-10-29 04:06:00 -0400

    :sparkles: remove dummy code

--- src/screens/mainStack/GiftedChat/index.js
+++ src/screens/mainStack/GiftedChat/index.js
@@ -1,80 +1,13 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { compose, withApollo } from 'react-apollo';
 import { withNavigation } from 'react-navigation';
-import { GiftedChat } from 'react-native-gifted-chat';
 import { ChatBot } from 'aws-amplify-react-native';
 
 import HeaderBar from '../../../components/HeaderBar';
 import styles from './styles';
 
-const lexRunTime = new AWS.LexRuntime();
-
 class Chat extends React.Component {
-  state = {
-    messages: [],
-  };
-
-  componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-      ],
-    });
-  }
-
-  // showRequest = inputText => {
-  //   // Add text input to messages in state
-  //   this.setState(prevState => ({
-  //     messages: GiftedChat.append(prevState.messages, messages),
-  //   }));
-  //   this.sendToLex(inputText);
-  // };
-
-  // handleTextSubmit = message => {
-  //   const inputText = message.trim();
-  //   if (inputText !== '') this.showRequest(inputText);
-  // };
-
-  // sendToLex = message => {
-  //   const params = {
-  //     botAlias: '$LATEST',
-  //     botName: 'Your-Bot-Name',
-  //     inputText: message,
-  //     userId: lexUserId,
-  //   };
-  //   lexRunTime.postText(params, (err, data) => {
-  //     if (err) {
-  //       // TODO SHOW ERROR ON MESSAGES
-  //     }
-  //     if (data) {
-  //       this.showResponse(data);
-  //     }
-  //   });
-  // };
-
-  // showResponse = lexResponse => {
-  //   const lexMessage = lexResponse.message;
-  //   this.setState(prevState => ({
-  //     messages: GiftedChat.append(prevState.messages, lexMessage),
-  //   }));
-  // };
-
-  onSend = (messages = []) => {
-    this.setState(prevState => ({
-      messages: GiftedChat.append(prevState.messages, messages),
-    }));
-  };
-
   handleComplete(err, confirmation) {
     if (err) {
       alert('Bot conversation failed');
@@ -86,20 +19,9 @@ class Chat extends React.Component {
   }
 
   render() {
-    const { messages } = this.state;
-
     return (
       <SafeAreaView style={styles.safeContainer}>
         <HeaderBar heading="Chat" {...this.props} />
-        {/* <GiftedChat
-          messages={messages}
-          onSend={messages => this.onSend(messages)}
-          renderActions={this.renderActions}
-          alwaysShowSend
-          user={{
-            _id: 1,
-          }}
-        /> */}
         <ChatBot
           title="My Bot"
           botName="ScheduleAppointment_devv"
